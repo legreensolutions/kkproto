91ea1ec3b9b15cfede97ce618e3cafa23df2e516
sujithsurendran <sujithsurendran@yahoo.com>
2013-01-09 11:15:56 +0530

    <b> removed, long rows split into two

--- plugins/user/signup/form/signup.php
+++ plugins/user/signup/form/signup.php
@@ -21,7 +21,7 @@
                 </tr>
                 <tr>
                     <td valign="bottom" align="right" class="passwd_caption" width="50%">
-                        <b><?php echo $CAP_username ?></b>
+                        <?php echo $CAP_username ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="text" name="txtusername" id="txtusername" value="<?php if(isset($_POST['txtusername'])){echo $_POST['txtusername'];}?>" >
@@ -30,7 +30,7 @@
                 <tr><td></td><td><div id="results"></div></td></tr>
                  <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_password ?></b>
+                        <?php echo $CAP_password ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="password" name="txtpasswd" id="txtpasswd" value="<?php if(isset($_POST['txtpasswd'])){echo $_POST['txtpasswd'];}?>" ></td>
@@ -38,7 +38,7 @@
 
                 <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_repassword ?></b>
+                        <?php echo $CAP_repassword ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="password" name="txtrepasswd" id="txtrepasswd" value="<?php if(isset($_POST['txtrepasswd'])){echo $_POST['txtrepasswd'];}?>" ></td>
@@ -51,7 +51,7 @@
 
                 <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%">
-                        <b><?php echo $CAP_firstname ?></b>
+                        <?php echo $CAP_firstname ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="text" name="txtfirstname" id="txtfirstname" value="<?php if(isset($_POST['txtfirstname'])){echo $_POST['txtfirstname'];}?>" ></td>
@@ -59,7 +59,7 @@
 
                <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_lastname ?></b>
+                        <?php echo $CAP_lastname ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="text" name="txtlastname" id="txtlastname" value="<?php if(isset($_POST['txtlastname'])){echo $_POST['txtlastname'];}?>" >
@@ -68,7 +68,7 @@
 
                 <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_address ?></b>
+                        <?php echo $CAP_address ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="text" name="txtaddress" id="txtaddress" value="<?php if(isset($_POST['txtaddress'])){echo $_POST['txtaddress'];}?>" >
@@ -77,7 +77,7 @@
 
                  <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_city ?></b>
+                        <?php echo $CAP_city ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <input class="passwd_box"  type="text" name="txtcity" id="txtcity" value="<?php if(isset($_POST['txtcity'])){echo $_POST['txtcity'];}?>" >
@@ -86,7 +86,7 @@
 
                 <tr>
                     <td valign="bottom" align="right" class="passwd_caption"  width="50%" >
-                        <b><?php echo $CAP_country ?></b>
+                        <?php echo $CAP_country ?>
                     </td>
                     <td valign="top" align="left"  width="50%">
                     <?php if(isset($_POST['lstcountry'])){ $value = $_POST['lstcountry']; } else{ $value = -1;}
@@ -94,15 +94,19 @@
                   ?>
                   </td>
                 </tr>
-                <tr colspan="2">
-                    <td  valign="bottom" align="right" class="passwd_caption"  width="50%">
-                        <b><?php echo $CAP_image ?></b>
-<input type="file" name="fleimage" id="fleimage" size="30" />
+                <tr>
+                    <td  valign="bottom" align="right" class="passwd_caption" >
+                          <?php echo $CAP_image ?><br />
+                          <input type="file" name="fleimage" id="fleimage" size="30" />
+                          
                     </td>
                 </tr>
+
+
+
                 <tr>
                     <td  width="50%" align="right">
-                        <b><?php echo $CAP_sec_que ?></b>
+                        <?php echo $CAP_sec_que ?>
                     </td>
                     <td  width="50%">
                          <?php if(isset($_POST['lstsec_que'])){ $value = $_POST['lstsec_que']; } else{ $value = -1;}
@@ -111,14 +115,14 @@
                 </tr>
                 <tr>
                     <td  width="50%" align="right">
-                        <b><?php echo $CAP_sec_ans ?></b>
+                        <?php echo $CAP_sec_ans ?>
                     </td>
                     <td  width="50%">
                         <input class="passwd_box"  type="text" name="txtsec_ans" id="txtsec_ans" value="<?php if(isset($_POST['txtsec_ans'])){echo $_POST['txtsec_ans'];}?>" />
                     </td>
                 </tr>
     <tr>
-        <td  width="50%" align="right"><b><?php echo $MSG_enter_security_code?></b></td>
+        <td  width="50%" align="right"><?php echo $MSG_enter_security_code?></td>
         <td  width="50%">
              <input id="security_code" name="security_code" type="text" />
         </td>
