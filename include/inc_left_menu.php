00266b9fff4e60593c10edc67a7ba92c5314ecc4
Pramod G. Menon <pramodgmenon@gmail.com>
2013-01-14 10:36:01 +0530

    main layout modified

--- include/inc_left_menu.php
+++ include/inc_left_menu.php
@@ -3,7 +3,6 @@
 <a href="about_us.php"><div id="m3">About us&nbsp;&nbsp;</div></a>
 <a href="faq.php"><div id="m4">FAQ&nbsp;&nbsp;</div></a>
 <a href="contact_us.php"><div id="m5">Contact us&nbsp;&nbsp;</div></a>
-<div id="m_bottom"></div>
 
-    <div id="bottom-left"></div>
-    <div id="bottom-left-repeat"></div>
+
+
